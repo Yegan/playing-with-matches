133b581b5d5939d042fdb4e8359cdf522bd45809
Yegan <yeganj@gmail.com>
2018-08-17 11:11:58 +0200

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-let displayRandom = document.querySelector('random')
+document.addEventListener("DOMContentLoaded", function (event) {
+    let displayRandom = document.querySelector('.random')
+    let button = document.querySelector('.clickButton')
 
 
 
+    function getRandom() {
+        let randomNum = Math.random();
+        displayRandom.innerHTML = randomNum
+    }
 
-function getRandom() {
-    let randomNum =  Math.random();
-      displayRandom.innerHTML = randomNum
-}
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; 
+      }
+
+    getRandom()
+getRandomInt()
+    button.addEventListener('click', function () {
+        getRandom()
+    });
+
+
+
+});
