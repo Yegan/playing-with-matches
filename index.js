2d82200061878e73bb03aef16083c24ad7293814
Yegan <yeganj@gmail.com>
2018-08-17 12:54:24 +0200

    improved logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    let button = document.querySelector('.clickButton')
+    //let button = document.querySelector('.clickButton')
     let numberElements = document.querySelectorAll('.num')
-
+   // let num = document.querySelector('.num')
 
     function arbitraryNum(min, max) {
 
@@ -14,15 +14,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
         let max = 9;
 
         for (let i = 0; i < numberElements.length; i++) {
+            console.log(numberElements)
             let numberElement = numberElements[i]
             numberElement.innerHTML = arbitraryNum(min, max)
+           // numberElement.value = arbitraryNum(min, max)
+            //numberElement.value = arbitraryNum(min, max)
+            console.log(numberElement.value)
+
+            if(numOne == numTwo){
+                if(numTwo == numThree)
+                classList.add('.yellow')
+
+            }
         }
+
     }
 
     getRandomArbitrary()
 
-    button.addEventListener('click', function () {
-        getRandomArbitrary()
-    });
+    // button.addEventListener('click', function () {
+    //     getRandomArbitrary()
+    // });
 
 });
