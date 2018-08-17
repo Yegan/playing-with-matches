71644129079fdb3dc9dd8ca03a547f4eec363e8b
Yegan <yeganj@gmail.com>
2018-08-17 14:28:45 +0200

    added message and counter

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     //let button = document.querySelector('.clickButton')
     let numberElements = document.querySelectorAll('.num')
+
+    var users = localStorage.getItem('users');
+
+    var storedUsers = users ? JSON.parse(users): {};
+
+
+    let count = document.querySelector('.counter')
+    let theMessage = document.querySelector('.message')
    // let num = document.querySelector('.num')
+
+   let counter = 0;
 
     function arbitraryNum(min, max) {
 
@@ -27,6 +37,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         if (numOne.innerHTML == numTwo.innerHTML) {
             numOne.classList.add('yellow')
             numTwo.classList.add('yellow')
+            theMessage.innerHTML = 'row one and row two matched'
+            counter ++
         }
 
         // if (numOne.innerHTML == numThree.innerHTML) {
@@ -37,11 +49,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
         if (numTwo.innerHTML == numThree.innerHTML) {
             numTwo.classList.add('yellow')
             numThree.classList.add('yellow')
+            theMessage.innerHTML = 'row two and row three matched'
+
+            counter ++
         }
 
+        
 
+        count.innerHTML = counter
     }
-
+    
     getRandomArbitrary()
 
     // button.addEventListener('click', function () {
